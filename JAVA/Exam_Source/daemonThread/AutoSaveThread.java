bcc4b19955c6b063f2217b365ead11be5fdbed67
SEUNGHAN BAEK <skud113@naver.com>
2022-02-10 09:27:29 +0900

    Daemon Thread 예제 추가
    Daemon 스레드 공부 예제 추가

---
+++ JAVA/Exam_Source/daemonThread/AutoSaveThread.java
@@ -0,0 +1,1 @@
+package daemonThread;public class AutoSaveThread extends Thread {    public void save() {        System.out.println("작업 내용 저장 완료");    }    @Override    public void run() {        while (true) {            try {                Thread.sleep(1000);            } catch (InterruptedException e) {                break;            }            save();        }    }}
