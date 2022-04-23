4b875c26d5962a4d5238a1783e367c0ad8aff9c4
back-seung <skud113@naver.com>
2022-04-23 20:56:28 +0900

    자바스터디 10주차 - 스레드
        1. 스레드란?
        1-1. 스레드, 프로세스 정의
        1-2. 스레드 생성법 (Runnable, Thread)
    
        2. 스레드 상태
        2-1. 스레드 상태 개념

--- JAVA/Exam_Source/week_10/ExecuteExample.java
+++ JAVA/Exam_Source/week_10/ExecuteExample.java
@@ -1,1 +1,1 @@
-package week_10;public class ExecuteExample {}
+package week_10;public class ExecuteExample {    public static void main(String[] args) {        // Runnable을 사용하여 Thread 생성        RunnableExample runnable = new RunnableExample();        new Thread(runnable).start();        // Thread를 사용하여 Thread 생성        ThreadExample thread = new ThreadExample("t");        thread.start();    }}
