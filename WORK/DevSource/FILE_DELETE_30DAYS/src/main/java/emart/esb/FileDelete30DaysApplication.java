43edaacf328c1bbbe34e5e13bc5e0edb811ba5b6
SEUNGHAN BAEK <84169773+back-seung@users.noreply.github.com>
2022-04-27 16:54:59 +0900

    스케줄러, 지정된 폴더 내 모든 파일의 생성일자를 확인하여 당일과의 차이가 30일이 넘으면 삭제함

---
+++ WORK/DevSource/FILE_DELETE_30DAYS/src/main/java/emart/esb/FileDelete30DaysApplication.java
@@ -0,0 +1,13 @@
+package emart.esb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileDelete30DaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileDelete30DaysApplication.class, args);
+	}
+
+}
---
+++ WORK/DevSource/FILE_DELETE_30DAYS/src/test/java/emart/esb/FileDelete30DaysApplicationTests.java
@@ -0,0 +1,13 @@
+package emart.esb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileDelete30DaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
