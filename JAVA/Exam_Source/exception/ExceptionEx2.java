dbe70cad619997827ac32f4ee50ab8d9400672fd
back-seung <skud113@naver.com>
2022-05-11 23:35:33 +0900

    try-catch에서의 흐름 에제 파일 추가

--- JAVA/Exam_Source/exception/ExceptionEx2.java
+++ JAVA/Exam_Source/exception/ExceptionEx2.java
@@ -1,1 +1,1 @@
-package exception;public class ExceptionEx2 {}
+package exception;public class ExceptionEx2 {    public static void main(String[] args) {        System.out.println(1);        System.out.println(2);        try {            System.out.println(3);            System.out.println(0 / 0);            System.out.println(4);        } catch (ArithmeticException e) {            System.out.println(5);        } // try - catch 끝        System.out.println(6);    } // main 끝}
