e89e9cd5721331ddc4ff0785530fb46719d99c87
back-seung <skud113@naver.com>
2022-05-21 00:09:01 +0900

    week11 - abstract method add

---
+++ JAVA/Exam_Source/week_11/Transportation.java
@@ -0,0 +1,1 @@
+package week_11;public enum Transportation {    BUS(100), TRAIN(150), SHIP(100), AIRPLANE(300);    private final int BASIC_FARE;    Transportation(int BASIC_FARE) {        this.BASIC_FARE = BASIC_FARE;    }    public int fare() {        return BASIC_FARE;    }}
