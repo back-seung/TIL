c2022f5e0c55f184bbb7f2d2ec4a915732b83951
back-seung <skud113@naver.com>
2022-07-08 10:46:34 +0900

    Function 패키지 끝

--- JAVA/Exam_Source/lambda/HTest.java
+++ JAVA/Exam_Source/lambda/HTest.java
@@ -6,11 +6,17 @@ import static java.lang.Integer.toBinaryString;
 
 public class HTest {
     public static void main(String[] args) {
-        Function<String, Integer> f = s -> Integer.parseInt(s, 16);
+//        Function<String, Integer> f = s -> Integer.parseInt(s, 16);
+//        Function<Integer, String> g = i -> toBinaryString(i);
+//        Function<String, String> h = f.andThen(g);
+//        System.out.println(f.apply("FF"));
+//
+//        System.out.println(h.apply("FF"));
+
         Function<Integer, String> g = i -> toBinaryString(i);
-        Function<String, String> h = f.andThen(g);
-        System.out.println(f.apply("FF"));
-        
-        System.out.println(h.apply("FF"));
+        Function<String, Integer> f = (s) -> Integer.parseInt(s, 16);
+        Function<Integer, Integer> h = f.compose(g);
+
+        System.out.println(h.apply(2));
     }
 }
